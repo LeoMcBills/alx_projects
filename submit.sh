44c22606a12a5043f19fba6a70d12883c61ad29c
leomcbills <leokinyera9@gmail.com>
2025-02-26 15:12:39 +0000

    updata submitting script

--- submit.sh
+++ submit.sh
@@ -8,6 +8,8 @@ if [ -z "$1" ]; then
 fi
 
 # Staging updates and pushing to github
+# It should be noted that I initially pushed my code with the security token before this so I will not get any errors, however, if you get any errors, then you might need to write the complete the git push command
+
 git add .
 git commit -m "$1"
 git push
