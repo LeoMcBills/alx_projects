a024c0404e34117e94219094ebfda0b047feda68
LeoMcBills <leokinyera9@gmail.com>
2025-02-26 15:32:36 +0300

    automate git push

--- submit.sh
+++ submit.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Checking whether a commit message was provided
+if [ -z "$1" ]; then
+	echo "Error: No commit message provided."
+	echo "Usage: ./submit.sh \"your commit message\""
+	exit 1
+fi
+
+# Staging updates and pushing to github
 git add .
-git commit -m "update"
+git commit -m "$1"
 git push
