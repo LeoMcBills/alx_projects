b005478f046ad0d26cbb82f3ba429e5f0d12978c
LeoMcBills <leokinyera9@gmail.com>
2025-03-22 12:28:03 +0300

    used ternary expression

--- C_programming/basics.c
+++ C_programming/basics.c
@@ -9,6 +9,12 @@ int main(int ac, char **av, char **env){
 	int my_array[32];
 	char my_array_of_arrays[8][16];
 
+	int a = 12;
+
+	a? 12: 14;
+
+	printf("Result of the ternary condition above is %d\n", a);
 	printf("My array has been created!\n");
+
 	return (0);
 }
