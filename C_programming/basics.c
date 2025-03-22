9c02c3ac48a82a8ae9b86eae89d29cc76acb160c
LeoMcBills <leokinyera9@gmail.com>
2025-03-22 12:07:21 +0300

    created my array

---
+++ C_programming/basics.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main: create array and slice it
+ * Return: Success
+ */
+
+int main(void){
+	int my_array[32];
+	char my_array_of_arrays[8][16];
+
+	printf("My array has been created!\n");
+	return (0);
+}
